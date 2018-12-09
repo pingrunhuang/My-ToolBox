fdf3ec7956db21a11ae3af53c4604df7221e379f
frank <pingrunhuang@126.com>
2018-12-09 17:09:26 +0800

    restructure

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# My-ToolBox
-This repo contains my solution for different scenarios
 
-## What is the purpose for this repo?
-As I want to automate the process of deployment, I think it would be greate to build up my own tool box so that I do not need to google for the solution every time. What's more important is that it would be a greate idea to keep the record to be a devops.
-
+### positional parameters
+`$#` return the number of parameters.  
+`$1` get the first argument value. Same goes to 2 to 9.  
+`${10}` get the tenth argument value. For historical reasons, have to add {} to later position.  
+`$*` Represents all the command-line arguments as a single string which can be iterated by `for i in $*`. Same as `$@` With quotes, `"$*"` represents the whole string which can not be iterated.
+`$?` Exit status of previous command. 0 means success. 126 means Command found, but file was not executable. 127 means Command not found. > 128 means Command died due to receiving a signal.
---
+++ setup_accumulo.sh
@@ -0,0 +1,38 @@
+# ########### ACCUMULO #############
+# https://accumulo.apache.org/downloads/
+# requires: hdfs, zookeeper
+
+# change this
+cd $WORK_DIR=/Users/runpinghuang/dev
+
+# change if necessary
+export VERSION=1.9.2
+wget http://www.mirrorservice.org/sites/ftp.apache.org/accumulo/$VERSION/accumulo-$VERSION-bin.tar.gz
+tar -xzvf accumulo-$VERSION-bin.tar.gz
+
+echo " " >> ~/.bashrc
+echo "# ======== ACCUMULO ======== #" >> ~/.bashrc
+echo "export ACCUMULO_HOME=$WORK_DIR/accumulo-$VERSION" >> ~/.bashrc
+echo 'export PATH=$PATH:$ACCUMULO_HOME/bin' >> ~/.bashrc
+
+source ~/.bashrc
+
+cd accumulo-$VERSION
+
+
+./bin/build_native_library.sh
+cp conf/examples/1GB/standalone/* conf/
+
+# to be able to access it over the Internet, you have to set the value
+# of ACCUMULO_MONITOR_BIND_ALL to true.
+sed 's/# export ACCUMULO_BIND_ALL/export ACCUMULO_BIND_ALL/' < conf/accumulo-env.sh > conf/accumulo-env.test.sh
+rm conf/accumulo-env.sh
+mv conf/accumulo-env.test.sh conf/accumulo-env.sh
+
+chmod 700 conf/accumulo-env.sh
+
+# instance.volumes: where to store the data on HDFS
+perl -0777 -i.original -pe 's@<name>instance.volumes</name>\n\s+<value></value>@<name>instance.volumes</name>\n<value>hdfs://lo    calhost:8020/accumulo</value>@igs' conf/accumulo-site.xml
+
+# change instance.secret
+perl -0777 -i.original -pe 's@<value>test</value>@<value>test</value>@igs' conf/accumulo-site.xml
